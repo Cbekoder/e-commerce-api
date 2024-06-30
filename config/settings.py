418e5bbd284438752f1c96e46de6c349e35eb0e9
cbekoder <cbekoder@gmail.com>
2024-07-01 02:02:31 +0500

    push for deploy configured

--- config/settings.py
+++ config/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ko6p%eydtohk$u_7r1jsij#ew@l%*sg5r5g3cdl8g3z@e)=g=7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -147,7 +147,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+# if DEBUG:
 STATIC_ROOT = BASE_DIR / 'static'
+
+# STATICFILES_DIRS = BASE_DIR / 'static'
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
