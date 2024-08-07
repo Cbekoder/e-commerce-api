2e4c167becde518296aec1f23c4a38d4edcf9a2f
cbekoder <cbekoder@gmail.com>
2024-08-07 18:50:51 +0500

    update: adding is_liked column in products list

--- product/serializers.py
+++ product/serializers.py
@@ -1,32 +1,51 @@
 from rest_framework.serializers import ModelSerializer, SerializerMethodField
+
+from action.models import Wishlist
 from .models import Category, Product, ProductPictures, ProductProperty
+
 
 class CategorySerializer(ModelSerializer):
     class Meta:
         model = Category
         fields = ['id', 'title']
 
+
 class ProductPicturesSerializer(ModelSerializer):
     class Meta:
         model = ProductPictures
         fields = ['file', 'as_main']
+
 
 class ProductPropertySerializer(ModelSerializer):
     class Meta:
         model = ProductProperty
         fields = ['key', 'value']
 
+
 class ProductSerializer(ModelSerializer):
     pictures = SerializerMethodField()
+    is_liked = SerializerMethodField()
 
     class Meta:
         model = Product
-        fields = ['id', 'title', 'price', 'discount_percent', 'discount_price', 'review_quantity', 'stars', 'pictures']
+        fields = ['id', 'title', 'price', 'discount_percent', 'discount_price', 'review_quantity', 'stars', 'is_liked',
+                  'pictures']
 
     def get_pictures(self, obj):
         pictures = obj.productpictures_set.all()
         sorted_pictures = sorted(pictures, key=lambda x: not x.as_main)
         return [picture.file.url for picture in sorted_pictures]
+
+    def get_is_liked(self, obj):
+        request = self.context.get('request', None)
+        if request and hasattr(request, 'user'):
+            return Wishlist.objects.filter(user=request.user, product=obj).exists()
+        return False
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        representation['is_liked'] = self.get_is_liked(instance)
+        return representation
 
 
 class ProductDetailSerializer(ModelSerializer):
@@ -35,7 +54,8 @@ class ProductDetailSerializer(ModelSerializer):
 
     class Meta:
         model = Product
-        fields = ['id', 'title', 'description', 'category', 'price', 'discount_percent', 'discount_price', 'quantity', 'stars', 'review_quantity', 'pictures', 'properties']
+        fields = ['id', 'title', 'description', 'category', 'price', 'discount_percent', 'discount_price', 'quantity',
+                  'stars', 'review_quantity', 'pictures', 'properties']
 
     def get_pictures(self, obj):
         pictures = obj.productpictures_set.all()
